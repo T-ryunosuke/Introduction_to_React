2c6f886b9b0d7886594fd8724d0d4807934300e8
Ryunosuke <dog.kira@yahoo.ne.jp>
2025-01-05 12:50:45 +0900

    講座253終了時点
    内容：型推論について

--- 20_typescript/src/060_type_inference/start/Example.tsx
+++ 20_typescript/src/060_type_inference/start/Example.tsx
@@ -1,5 +1,11 @@
 const Example = () => {
   // 練習用
+  let str = "hello"
+  let obj = { name: "Taro", age: 10 };
+
+  // constはリテラル型として推測される
+  const bye = "bye";
+  const num = 123;
 };
 
 export default Example;
