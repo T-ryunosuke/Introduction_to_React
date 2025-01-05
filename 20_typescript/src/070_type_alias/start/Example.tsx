9176f3ebbf4f37a1e7a48d399e554fc861790da9
Ryunosuke <dog.kira@yahoo.ne.jp>
2025-01-05 13:06:44 +0900

    講座254終了時点
    内容：独自に型を定義する方法について

--- 20_typescript/src/070_type_alias/start/Example.tsx
+++ 20_typescript/src/070_type_alias/start/Example.tsx
@@ -1,5 +1,27 @@
 const Example = () => {
   // 練習用
+  type User = {
+    name: string,
+    age: number
+  }
+  const user: User = {
+    name: "Taro", age: 10
+  }
+
+  // 単独の型も別名で定義可能
+  type UserName = string;
+  type UserGender = "man" | "woman" | "others";
+
+  // 定義した型エイリアスを使ってさらに別の型を定義することも可能
+  type UserProfile = {
+    name: UserName;
+    gender: UserGender;
+  };
+
+  const userProfile: UserProfile = {
+    name: "Hanako",
+    gender: "woman",
+  };
 };
 
 export default Example;
